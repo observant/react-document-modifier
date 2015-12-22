7138ab3a7c2af7cd3d939f2e7f23e9fb96e35384
Hadi Michael <hadimichael@gmail.com>
2015-12-22 14:01:32 +1100

    Enhance tests to check for children support

--- __tests__/documentModifier.spec.js
+++ __tests__/documentModifier.spec.js
@@ -6,7 +6,7 @@ import React from 'react';
 import ReactTestUtils from 'react-addons-test-utils';
 import DocumentModifier from '../dist/index.js';
 
-describe(new Date() + '\nDocumentModifier component', () => {
+describe(new Date() + '\nDocumentModifier core functionality', () => {
 	beforeEach(() => {
 		//clear properties we will be testing for
 		//we have to do this, because document is persistent and wouldn't be cleared with unmount
@@ -81,5 +81,86 @@ describe(new Date() + '\nDocumentModifier component', () => {
 		});
 
 		ReactTestUtils.renderIntoDocument(React.createElement(ParentComponent));
+	});
+});
+
+describe(new Date() + '\nDocumentModifier component', () => {
+	before(() => {
+		console.error = (error) => {
+			//escalate Warnings to Errors
+			//We do this to manually catch Failed propType warnings
+			throw new Error(error);
+		};
+	});
+
+	it('requires `properties` prop', (done) => {
+		const wrapperFn = () => {
+			const Component = React.createClass({
+				render: () => {
+					return (
+						<DocumentModifier />
+					);
+				},
+			});
+
+			ReactTestUtils.renderIntoDocument(React.createElement(Component));
+		};
+
+		expect(wrapperFn).to.throw(Error);
+		done();
+	});
+
+	it('can accept no children', (done) => {
+		const Component = React.createClass({
+			componentDidMount: () => {
+				done();
+			},
+			render: () => {
+				return (
+					<DocumentModifier properties={{title: 'unit test'}} />
+				);
+			},
+		});
+
+		ReactTestUtils.renderIntoDocument(React.createElement(Component));
+	});
+
+	it('can accept exactly one child', (done) => {
+		const Component = React.createClass({
+			componentDidMount: () => {
+				done();
+			},
+			render: () => {
+				return (
+					<DocumentModifier properties={{title: 'unit test'}}>
+						<p>Exactly one child!</p>
+					</DocumentModifier>
+				);
+			},
+		});
+
+		ReactTestUtils.renderIntoDocument(React.createElement(Component));
+	});
+
+	it('can accept multiple children', (done) => {
+		const Component = React.createClass({
+			componentDidMount: () => {
+				done();
+			},
+			render: () => {
+				return (
+					<DocumentModifier properties={{title: 'unit test'}}>
+						<p>My first paragraph</p>
+						<p>... and another paragraph element.</p>
+						<ul>
+							<li>Let's throw</li>
+							<li>a list into the mix</li>
+						</ul>
+					</DocumentModifier>
+				);
+			},
+		});
+
+		ReactTestUtils.renderIntoDocument(React.createElement(Component));
 	});
 });
